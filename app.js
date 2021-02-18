2064af382635d079789eee5fbde2d576b4a5088c
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2021-02-19 04:19:16 +0700

    adding socket io to server

--- app.js
+++ app.js
@@ -10,9 +10,21 @@ const io = require('socket.io')(http, {
   allowEIO3: true
 })
 
+let users = []
+
 io.on('connection', (socket) => {
+  // add logged-in username to users array
+  
   // Broadcast when a user connects
-  socket.broadcast.emit('message', 'A user has joined the game')
+  socket.on('newUser', (username) => {
+    users.push(username)
+    console.log('ini user:', users)
+    io.emit('userOnline', users)
+  })
+
+  // socket.emit('loggedIn', {
+  //   users: users.map(data => data.username)
+  // })
 
   // Broadcast when a user typing the keyboard
   socket.on('newTyping', (typing) => {
@@ -21,11 +33,23 @@ io.on('connection', (socket) => {
 
   // Broadcast when a user wins
   socket.on('UserWinner', (message) => {
-    socket.broadcast.emit('serverUserWinner', message)
+    io.emit('serverUserWinner', message)
   })
 
-  // Run when a user disconnects
-  socket.on('disconnect', () => {
+  socket.on("shareTyping", (text) => {
+    socket.broadcast.emit("enemyText", text);
+  })
+
+  socket.on("removeUser", (data) => {
+    let newUsers = users.filter(el => {el !== data})
+    users.splice(users.indexOf(data), 1)
+    io.emit('afterLogout', users)
+
+    users = newUsers
+  })
+
+   // Run when a user disconnects
+   socket.on('disconnect', () => {
     io.emit('message', 'A user has left the game')
   })
 })
