4bae0238a8f793f47714da6b09081d1f18702dbd
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2021-02-19 13:41:42 +0700

    edit origin client

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http').Server(app);
 
 const io = require('socket.io')(http, {
   cors: {
-    origin: "http://localhost:8081",
+    origin: "https://keyboard-smasher-d4345.web.app",
     methods: ["GET", "POST"],
     credentials: true
   },
